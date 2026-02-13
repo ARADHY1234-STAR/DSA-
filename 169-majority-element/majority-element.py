26a13d09c42190705fd21af1e73b4d03e49d06b2
ARADHY1234-STAR <aradhyshrivastava890@gmail.com>
2026-02-13 17:11:23 +0530

    Time: 3 ms (85.44%) | Memory: 21.2 MB (57.60%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,31 +1,18 @@
 from typing import List
 class Solution(object):
    def majorityElement(self, nums):
-       n = len(nums)
-       count = 1
-       majority = nums[0]
+     freq = 0
+     ans = 0
+     n = len(nums)
+     
+     for i in range(n):
+        
+        if (freq ==0):
+            ans = nums[i]
+        
+        if ans == nums[i]:
+            freq +=1
+        else:
+            freq -=1       
 
-
-       # Phase 1: Find potential majority candidate
-       for i in range(1, n):
-           if nums[i] == majority:
-               count += 1
-           else:
-               count -= 1
-               if count == 0:
-                   majority = nums[i]
-                   count = 1
-
-
-       # Phase 2: Verify the candidate
-       count = 0
-       for i in range(n):
-           if nums[i] == majority:
-               count += 1
-
-
-       # Return candidate if it occurs more than n/2 times
-       if count > n // 2:
-           return majority
-       else:
-           return -1  # No majority element found
+     return ans
