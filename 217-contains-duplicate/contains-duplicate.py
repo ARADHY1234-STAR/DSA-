9734d13db19ffb5979d16aaab711bc28122652f0
ARADHY1234-STAR <aradhyshrivastava890@gmail.com>
2026-04-01 09:18:15 +0530

    Time: 19 ms (59.29%) | Memory: 25.9 MB (66.95%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,6 +1,11 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        if len(set(nums)) == len(nums):
-            return False
-        else:
-            return True    
+class Solution(object):
+    def containsDuplicate(self, nums):
+      
+      hashset = set()
+
+      for i in nums:
+        if i in hashset:
+            return True
+        hashset.add(i)
+      return False      
+        
